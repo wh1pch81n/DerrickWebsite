3e3798733cabdae358a3e8c08852b9f55427ea0e
wh1pch81n <wh1pch81n@gmail.com>
2013-12-09 23:07:05 -0800

    insert the selection list for the tutorial names

--- teaching.js
+++ teaching.js
@@ -1,17 +1,33 @@
-function initContent(parent) {
+var globalTutorialName = null;
+var globalLessonNum = null;
+var globalSlideNum = null;
 
-	
-	var slid = "John";
+function initContent(parent) {
+	var selectionListID = "tutorialNameID";
 	var optionArr = [
-	new OptionDetail("white", "white", true),
-	new OptionDetail("black", "black", false)
+	new OptionDetail("",""),
+	new OptionDetail("JavaScript", "JavaScript"),
+	new OptionDetail("VHDL", "VHDL")
 	];
-	var selectList = makeSelectionList(slid, optionArr);
+	var selectList = makeSelectionList(selectionListID, optionArr);
 	
 	mk("label", {for:"jk"}, parent, function(p) {
-		p.innerHTML = "Select background color";
+		p.innerHTML = "Select name of tutorial";
 	});
-
+	selectList.onchange = function() {
+		var selectObj = document.getElementById(selectionListID);
+		var index = selectObj.selectedIndex;
+		var tutorial = selectObj[index].value;
+		
+		globalTutorialName = tutorial; 
+		//come back to this later.  I'm confusing myself about what to do
+	};
 	parent.appendChild(selectList);
+};
+
+function handleTutorial() {
+	//alert(e["name"]);
+//	var selectObj = document.getElementById()
+
 };
 
