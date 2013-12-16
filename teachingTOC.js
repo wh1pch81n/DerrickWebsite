3a1365c038284c4e8691d1570d3d424b7fdc0056
wh1pch81n <wh1pch81n@gmail.com>
2013-12-15 16:04:19 -0800

    Moving globalTutorialArray to separate file

---
+++ teachingTOC.js
@@ -0,0 +1,22 @@
+/*
+ And array of tutorial objects that hold references to
+ the lessons
+ 
+ Used Extensivly by teaching.js
+ */
+var globalTutorialArr =
+[{
+tutorial:"VHDL",
+lessons:[
+	{value:vhdlLesson1, text:"1 - First Program"},
+		 //{value:vhdlLesson2, text:"VHDL Lesson 2"}
+		 ]
+},{
+tutorial:"JavaScript",
+tutorialFolder:"http://derrickho.co.nf/tutorialJavaScript/",
+lessons:[
+	{value:javaScriptLesson1, text:"1 - Strings, Arrays & Math"},
+	{file:"javaScriptL2.txt", text:"2 - ifs & loops"},
+		 //{file:"javaScriptL3.txt", text:"JavaScript Lesson 3"}
+		 ]
+}];
