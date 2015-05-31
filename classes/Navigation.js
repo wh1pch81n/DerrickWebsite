ff22b2434ac81d57f5d3badf2491565532675838
Derrick Ho <derrickho328@gmail.com>
2015-05-31 00:47:15 -0700

    - adding link to blog - using Currying to reduce code duplication

--- classes/Navigation.js
+++ classes/Navigation.js
@@ -24,35 +24,44 @@ function Navigation(){
 		footer.innerHTML = "Copyright &copy; 2013-present. All rights reserved";
 	}
 	this.initGUI = function() {
+		var navLinker = function(isExternal) {
+			return function(linkName, url) {
+				return new NavLink(linkName, url, isExternal);
+			};
+		};
+		var navInternal = navLinker(0);
+		var navExternal = navLinker(1);
 		var arrLinks = [
-										new NavLink("About Me", "index.html", 0),
-										new NavLink("Classes", "courses.html", 0),
-										new NavLink("Projects", "projects.html", 0),
-										new NavLink("Skills", "skills.html", 0),
-										new NavLink("Work History", "work_history.html", 0),
-										new NavLink("Teaching", "teaching.html", 0),
-										new NavLink("Project Euler", "https://github.com/wh1pch81n/ProjectEulerNew", 1),
-										new NavLink("CodeEval", "https://www.codeeval.com/public/8204d6d519fd06ef4ef36415ec9001466e87fc35/", 1),
-										new NavLink("<img src=\"http://s.c.lnkd.licdn.com/scds/common/u/img/webpromo/btn_in_20x15.png\" width=\"20\" height=\"15\" alt=\"View Derrick Ho's LinkedIn profile\">", "http://www.linkedin.com/in/derrickho88", 1)
-										];
+			navInternal("About Me", "index.html"),
+			navExternal("Blog", "http://embsi.blogspot.com"),
+			navInternal("Classes", "courses.html"),
+			navInternal("Projects", "projects.html"),
+			navInternal("Skills", "skills.html"),
+			navInternal("Work History", "work_history.html"),
+			navInternal("Teaching", "teaching.html"),
+			navExternal("Project Euler", "https://github.com/wh1pch81n/ProjectEulerNew"),
+			navExternal("CodeEval", "https://www.codeeval.com/public/8204d6d519fd06ef4ef36415ec9001466e87fc35/"),
+			navExternal("<img src=\"http://s.c.lnkd.licdn.com/scds/common/u/img/webpromo/btn_in_20x15.png\" width=\"20\" height=\"15\" alt=\"View Derrick Ho's LinkedIn profile\">", "http://www.linkedin.com/in/derrickho88")
+		];
 		
 		var nav = document.getElementById(this.navigationID);
-		var selNum = 0;
 		for(var i = 0; i < arrLinks.length; ++i) {
-			var p = arrLinks[i].createLinkElement();
-			
-			//check if current url is the link we are making
-			//if so give it the class "selected"
-			if(document.URL.indexOf(arrLinks[i].url) >= 0) {
-				p.setAttribute("class", "selected");
-				selNum = i;
-			}
-			nav.appendChild(p);
-		}
-		if(selNum == 0) {
-			nav.firstChild.setAttribute("class", "selected");
+			nav.appendChild(arrLinks[i].createLinkElement());
 		}
 		
+		//check if current url is the link we are making
+		//if so give it the class "selected"
+
+		var currentLink = [];
+		for (var i = 0; i < nav.childNodes.length; ++i) {
+			if (document.URL.indexOf(arrLinks[i].url) != -1) {
+				currentLink.push(nav.childNodes[i]);
+			}
+		}
+		if (currentLink.length) {
+			var p = currentLink[0];
+			p.setAttribute("class", "selected");
+		}
 	}
 };
 
